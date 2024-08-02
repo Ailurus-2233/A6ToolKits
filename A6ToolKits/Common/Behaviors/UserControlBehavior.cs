61d22940eb390c31c5dc25bc34dc76c5dcf8a610
Zyu_Work <ailurus2233@outlook.com>
2024-08-02 17:31:34 +0800

    添加控件事件行为

---
+++ A6ToolKits/Common/Behaviors/UserControlBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace A6ToolKits.Common.Behaviors;
+
+public class UserControlBehavior
+{
+    public static readonly AttachedProperty<ICommand> LoadedCommandProperty =
+        AvaloniaProperty.RegisterAttached<UserControlBehavior, Control, ICommand>("LoadedCommand");
+
+    static UserControlBehavior()
+    {
+        LoadedCommandProperty.Changed.Subscribe(OnLoadedCommandChanged);
+    }
+
+    public static ICommand GetLoadedCommand(Control element)
+    {
+        return element.GetValue(LoadedCommandProperty);
+    }
+
+    public static void SetLoadedCommand(Control element, ICommand value)
+    {
+        element.SetValue(LoadedCommandProperty, value);
+    }
+
+    private static void OnLoadedCommandChanged(AvaloniaPropertyChangedEventArgs<ICommand> e)
+    {
+        if (e.Sender is not Control control) return;
+        control.AttachedToVisualTree -= Control_Loaded;
+        control.AttachedToVisualTree += Control_Loaded;
+    }
+
+    private static void Control_Loaded(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        if (sender is not Control control) return;
+        var command = GetLoadedCommand(control);
+        command?.Execute(null);
+    }
+}
