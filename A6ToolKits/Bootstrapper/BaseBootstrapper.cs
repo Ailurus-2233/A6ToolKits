cc3b6b4d5de0616ed24100c2d55c190a93442531
Zyu_Work <ailurus2233@outlook.com>
2024-09-03 14:26:39 +0800

    设计 Bootstrapper 接管 Avalonia 应用的启动流程
    注：这里固定死了启动生命周期模式为 ClassicDesktopStyleApplicationLifetime，之后应该会有所修改

---
+++ A6ToolKits/Bootstrapper/BaseBootstrapper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using A6ToolKits.Bootstrapper.Interfaces;
+using A6ToolKits.Bootstrapper.Utils;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace A6ToolKits.Bootstrapper;
+
+public class BaseBootstrapper<TApplication, TWindow> : IBootstrapper
+    where TApplication : Application, new()
+    where TWindow : Window, new()
+{
+    private AppBuilder? _builder = null;
+    private string[]? _runArgs;
+    private ClassicDesktopStyleApplicationLifetime? _lifetime;
+
+    protected TWindow? MainWindow { get; set; }
+    
+    private string[] RunArgs
+    {
+        get => _runArgs ?? [];
+        set => _runArgs = value;
+    }
+
+    public virtual void Initialize()
+    {
+    }
+
+    public virtual void Configure()
+    {
+        _builder = AppBuilder.Configure<TApplication>().UsePlatformDetect().WithInterFont().LogToTrace();
+        _lifetime = LifetimeExtensions.PrepareLifetime(_builder, RunArgs, null);
+        _builder.SetupWithLifetime(_lifetime);
+    }
+
+    public virtual void OnCompleted()
+    {
+        if (_lifetime == null) return;
+        MainWindow ??= new TWindow();
+        _lifetime.MainWindow = MainWindow;
+        _lifetime.Start(RunArgs);
+    }
+
+
+    public virtual void Run(string[] args)
+    {
+        RunArgs = args;
+        Initialize();
+        Configure();
+        OnCompleted();
+    }
+}
---
+++ A6ToolKits/Bootstrapper/LifetimeExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reflection;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
+
+namespace A6ToolKits.Bootstrapper;
+
+public static class LifetimeExtensions
+{
+    public static void SubscribeGlobalEvents(this ClassicDesktopStyleApplicationLifetime lifetime)
+    {
+        var subscribeGlobalEventsMethod = typeof(ClassicDesktopStyleApplicationLifetime).GetMethod(
+            "SubscribeGlobalEvents",
+            BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+        subscribeGlobalEventsMethod?.Invoke(lifetime, null);
+    }
+
+    public static ClassicDesktopStyleApplicationLifetime PrepareLifetime(
+        AppBuilder builder,
+        string[] args,
+        Action<IClassicDesktopStyleApplicationLifetime>? lifetimeBuilder)
+    {
+        var extensionsType = typeof(ClassicDesktopStyleApplicationLifetimeExtensions);
+        var prepareLifetimeMethod =
+            extensionsType.GetMethod("PrepareLifetime", BindingFlags.NonPublic | BindingFlags.Static);
+        
+        if (prepareLifetimeMethod == null)
+        {
+            throw new MissingMethodException("PrepareLifetime method not found.");
+        }
+        
+        return prepareLifetimeMethod.Invoke(null, [builder, args, lifetimeBuilder]) as
+            ClassicDesktopStyleApplicationLifetime ?? throw new InvalidOperationException();
+    }
+}
