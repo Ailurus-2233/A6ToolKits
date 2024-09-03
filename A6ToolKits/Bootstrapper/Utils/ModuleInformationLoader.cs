d85cb440172a4527bf82afb7e4a782903dc3eb79
Zyu_Work <ailurus2233@outlook.com>
2024-09-03 14:28:15 +0800

    1. 自定义两个异常；2. 添加模块配置文件加载器；

---
+++ A6ToolKits/Bootstrapper/Utils/ModuleInformationLoader.cs
@@ -0,0 +1,17 @@
+﻿using A6ToolKits.Bootstrapper.Interfaces;
+using static System.Text.Json.JsonSerializer;
+
+namespace A6ToolKits.Bootstrapper.Utils;
+
+/// <summary>
+/// 模块信息加载器，加载的目标文件为json文件
+/// </summary>
+public static class ModuleInformationLoader
+{
+    public static T? LoadModule<T>(string path = "module.json") where T : IModule
+    {
+        var text = System.IO.File.ReadAllText(path);
+        var module = Deserialize<T>(text);
+        return module;
+    }
+}
---
+++ A6ToolKits/Common/Exceptions/LoadModuleException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reactive.PlatformServices;
+
+namespace A6ToolKits.Common.Exceptions;
+
+/// <summary>
+/// 模块加载异常
+/// </summary>
+public class LoadModuleException(string message) : Exception(message);
