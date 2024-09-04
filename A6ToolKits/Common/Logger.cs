4a5cd60269a1802e49a5b331946b3d022be59934
Zyu_Work <ailurus2233@outlook.com>
2024-09-04 12:49:43 +0800

    调整 A6ToolKits 增加日志输出，删除一些多余的文件

---
+++ A6ToolKits/Common/Logger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using Serilog;
+using Serilog.Events;
+
+namespace A6ToolKits.Common;
+
+public static class Logger
+{
+    public static void InitializeConsoleLogger(LogEventLevel level = LogEventLevel.Information)
+    {
+        Log.Logger = level switch
+        {
+            LogEventLevel.Verbose => new LoggerConfiguration().WriteTo.Console().MinimumLevel.Verbose().CreateLogger(),
+            LogEventLevel.Debug => new LoggerConfiguration().WriteTo.Console().MinimumLevel.Debug().CreateLogger(),
+            LogEventLevel.Information => new LoggerConfiguration().WriteTo.Console().MinimumLevel.Information().CreateLogger(),
+            LogEventLevel.Warning => new LoggerConfiguration().WriteTo.Console().MinimumLevel.Warning().CreateLogger(),
+            LogEventLevel.Error => new LoggerConfiguration().WriteTo.Console().MinimumLevel.Error().CreateLogger(),
+            LogEventLevel.Fatal => new LoggerConfiguration().WriteTo.Console().MinimumLevel.Fatal().CreateLogger(),
+            _ => new LoggerConfiguration().WriteTo.Console().MinimumLevel.Information().CreateLogger()
+        };
+    }
+}
