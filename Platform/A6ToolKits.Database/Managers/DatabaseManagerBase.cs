2e2b19b9706919a14725a571372bcf2af2b9bcf7
Ailurus <Ailurus2233@outlook.com>
2024-12-02 17:37:12 +0800

    DatabaseModule 增加 SQLite 支持 （未完成）

---
+++ Platform/A6ToolKits.Database/Managers/DatabaseManagerBase.cs
@@ -0,0 +1,38 @@
+﻿using A6ToolKits.Database.DataModels;
+
+namespace A6ToolKits.Database.Managers;
+
+/// <summary>
+///     数据库管理器基类
+/// </summary>
+public abstract class DatabaseManagerBase : IManager
+{
+    /// <summary>
+    ///     连接字符串
+    /// </summary>
+    protected string? ConnectionString { get; set; }
+
+    /// <inheritdoc />
+    public abstract void Save<T>(IList<T> data, bool force = false) where T : IData;
+
+    /// <inheritdoc />
+    public abstract void Add<T>(IList<T> data) where T : IData;
+
+    /// <inheritdoc />
+    public abstract void Add<T>(params T[] data) where T : IData;
+
+    /// <inheritdoc />
+    public abstract IList<T> Load<T>() where T : IData;
+
+    /// <inheritdoc />
+    public abstract void Delete<T>(IList<T> target) where T : IData;
+
+    /// <inheritdoc />
+    public abstract void Delete<T>(params T[] target) where T : IData;
+
+    /// <inheritdoc />
+    public abstract void Update<T>(IList<T> data) where T : IData;
+
+    /// <inheritdoc />
+    public abstract void Update<T>(params T[] data) where T : IData;
+}
---
+++ Platform/A6ToolKits.Database/Managers/SQLiteDatabaseManager.cs
@@ -0,0 +1,60 @@
+﻿using A6ToolKits.Database.Configs;
+
+namespace A6ToolKits.Database.Managers;
+
+/// <summary>
+///     SQLite 数据库管理器基类
+/// </summary>
+public class SQLiteDatabaseManager : DatabaseManagerBase
+{
+    /// <summary>
+    ///     构造函数
+    /// </summary>
+    /// <param name="config">
+    ///     SQLite 配置项
+    /// </param>
+    public SQLiteDatabaseManager(SQLiteConfigItem config)
+    {
+        ConnectionString = config.ToConnectionString();
+    }
+    
+    public override void Save<T>(IList<T> data, bool force = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Add<T>(IList<T> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Add<T>(params T[] data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IList<T> Load<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Delete<T>(IList<T> target)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Delete<T>(params T[] target)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update<T>(IList<T> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update<T>(params T[] data)
+    {
+        throw new NotImplementedException();
+    }
+}
